dada5a7ded3448b78bac01e00d6f412df39fb272
somto <somto565@gmail.com>
2023-09-12 23:06:19 +0100

    integrating footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import fb from "../assets/fb.png"
+import tweet from "../assets/tweet.png"
+import youtube from "../assets/youtube.png"
+import insta from "../assets/insta.png"
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
