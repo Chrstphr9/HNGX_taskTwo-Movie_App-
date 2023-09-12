310c1f3c01f5857eed4dd4ea2921dfff768c938b
somto <somto565@gmail.com>
2023-09-12 17:39:03 +0100

    adding corasel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
-import Home from "./pages/Home"
+import { useEffect, useState } from "react";
+import Hero from "./components/Hero";
 
-
+const API_URL= " https://api.themoviedb.org/3/movie/popular?api_key=146776457697eb1d2f870174b4db5ee8";
 function App() {
   const [movies, setMovies]=useState([]);
+  
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res)=>res.json())
+    .then(data=> {
+      console.log(data);
+      setMovies(data.results)
+    })
+  }, [])
+  
 
   return (
     <>
-     {movies.map((moviesReq)=><Home />)}
+     {movies.map((movieReq)=>
+     <Hero key={movieReq.id} {...movieReq}/>)}
     </>
-    
+
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'; 
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Navbar from './Navbar';
+const API_IMG = "https://image.tmdb.org/t/p/w500/"
+const CAROUSEL_DELAY = 3000;
 
-const Hero = () => {
+
+const Hero = ({firstFiveMovies, setMovies}) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % firstFiveMovies.length)
+      })
+    
+    }, CAROUSEL_DELAY)
+    
+
+
+
   return (
-    <div>Hero</div>
+    <div>
+    <h1>{title}</h1>
+    <img src={API_IMG + poster_path} alt="nthn" />
+    <p>{overview}</p>
+    </div>
   )
 }
 
