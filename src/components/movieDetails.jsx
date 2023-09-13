339fc1577e1d70728815c3dcb6e8c6affc2f3f90
somto <somto565@gmail.com>
2023-09-13 16:21:13 +0100

    movie details functions

--- src/components/movieDetails.jsx
+++ src/components/movieDetails.jsx
@@ -1,9 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+// import tv from "../assets/tv.png"
+// import Home from "../assets/Home.png"
+// import movie from "../assets/movie.png"
+// import series from "../assets/series.png"
+// import calendar from "../assets/calendar.png"
+// import Logout from "../assets/Logout.png"
+import { Link } from 'react-router-dom';
+
+const API_KEY = "146776457697eb1d2f870174b4db5ee8"
+const API_URL = "https://api.themoviedb.org/3/movie/"
 
 const movieDetails = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [movieDetails, setMovieDetails] = useState(null)
+
+    useEffect(() => {
+      const fetchMOvieDetails = async () => {
+        try {
+            const response = await fetch(`${API_URL}${id}?api_key=${API_KEY}`);
+            if (!response.ok) {
+                throw new Error("Movie Not Found")
+            }
+            const data = await response.json();
+            setMovieDetails(data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+      };
+      fetchMOvieDetails();
+    }, [id]);
+    
+    const handleBackButtonClick = () => {
+        navigate(-1)
+    }
+
+    if (!movieDetails) {
+        return <div>Loading...</div>;
+    }
+
   return (
-    <div>movieDetails</div>
+    <div></div>
   )
 }
 
