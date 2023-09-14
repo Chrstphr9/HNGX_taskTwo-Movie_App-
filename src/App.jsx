b27d91aa32dedefccd2b7daa9429962053e5ad0c
somto <somto565@gmail.com>
2023-09-14 01:12:51 +0100

    rewriting the env file

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Footer from "./components/Footer";
 import MovieDetails from './components/movieDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=146776457697eb1d2f870174b4db5ee8";
+const movieUrl = "import.meta.env.VITE_MOVIEBOX_URL";
 function App() {
   const [movies, setMovies] = useState([]);
   const [firstFiveMovies, setFirstFiveMovies] = useState([])
 
   useEffect(() => {
-    fetch(API_URL)
+    fetch(movieUrl)
       .then((res) => res.json())
       .then(data => {
         console.log(data);
@@ -20,7 +20,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-    fetch(API_URL)
+    fetch(movieUrl)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -35,7 +35,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Hero firstFiveMovies={firstFiveMovies} movies={movies} setFirstFiveMovies={setFirstFiveMovies} setMovies={setMovies} />} />
       <Route path="/featured" element={<Featured movies={movies} setMovies={setMovies} />} />
-      {/* Use the :id parameter in the path */}
       <Route path="/movies/:id" element={<MovieDetails />} />
     </Routes>
     <Footer />
